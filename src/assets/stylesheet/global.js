5c1f7cbaa67f435b108927a0930593b44869f2c7
Kunj-Choksi <kunj07soni@gmail.com>
2022-02-07 09:27:11 -0500

    Partial dashboard screen

--- src/assets/stylesheet/global.js
+++ src/assets/stylesheet/global.js
@@ -17,6 +17,24 @@ const globalStyles = StyleSheet.create({
     textBold: {
         fontWeight: '600',
     },
+    text2XLarge: {
+        fontSize: 32,
+    },
+    textXLarge: {
+        fontSize: 28,
+    },
+    textLarge: {
+        fontSize: 24,
+    },
+    textMedium: {
+        fontSize: 20,
+    },
+    textSm: {
+        fontSize: 16,
+    },
+    textAlignCenter: {
+        textAlign: 'center',
+    },
     inputText: {
         borderWidth: 2,
         borderColor: '#d3d3d3',
@@ -26,6 +44,28 @@ const globalStyles = StyleSheet.create({
     },
     marB20: {
         marginBottom: 20,
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+    },
+    tag: {
+        padding: 15,
+        marginTop: 15,
+        borderWidth: 3,
+        borderColor: '#d3d3d3',
+        backgroundColor: '#e3e3e3',
+        borderRadius: 20,
+        green: {
+            borderColor: '#02f20e',
+            backgroundColor: '#7cf281',
+        },
+        red: {
+            borderColor: '#fc0303',
+            backgroundColor: '#ff6666',
+        },
     },
 });
 
--- src/components/analytics/Summery.js
+++ src/components/analytics/Summery.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import globalStyles from '../../assets/stylesheet/global';
 
 const Summery = () => {
     return (
-        <View style={styles.center}>
-            <Text>This is the Summery screen</Text>
+        <View style={globalStyles.flex1}>
+            <View style={[styles.header, globalStyles.textAlignCenter]}>
+                <Text style={[globalStyles.textLarge]}>
+                    Your this month gross
+                </Text>
+                <View style={[globalStyles.tag, globalStyles.tag.red]}>
+                    <Text style={globalStyles.text2XLarge}>CA$ 320</Text>
+                </View>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: 'center',
+    header: {
+        width: '100%',
+        marginTop: 20,
+        display: 'flex',
         alignItems: 'center',
-        textAlign: 'center',
     },
 });
 
